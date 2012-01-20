b1d9f33c3e8f0b9d60de74d378ee671cf3f1cfda
kashiwasan <pc.kashiwasan@gmail.com>
2012-01-20 17:52:47 +0900

    fixed conflicts
    	modified:   apps/api/config/routing.yml
    	modified:   apps/api/modules/member/actions/actions.class.php

--- apps/api/modules/member/actions/actions.class.php
+++ apps/api/modules/member/actions/actions.class.php
@@ -2,24 +2,12 @@
 
 class memberActions extends opApiActions
 {
-  public function executeCommunity(sfWebRequest $request)
+  public function executeCommunities(sfWebRequest $request)
   {
-    if (isset($request['member_id']))
-    {
-      $memberId = $request['member_id'];
-    }
-    elseif (isset($request['id']))
-    {
-      $memberId = $request['id'];
-    }
-    else
-    {
-      $memberId = $this->getUser()->getMemberId();
-    }
+    $memberId = isset($request['member_id']) ? $request['member_id'] : $this->getUser()->getMemberId();
 
     $this->communities = Doctrine::getTable('Community')->createQuery('c')
       ->innerJoin('c.CommunityMember cm WITH cm.is_pre = false AND cm.member_id = ?', $memberId)
-      ->limit(sfConfig::get('op_json_api_limit', 20))
       ->execute();
 
     $this->setTemplate('array', 'community');
