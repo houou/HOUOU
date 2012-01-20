26d0bf1ed5e5ac8d65ceb8655afa6bf9ec016209
kashiwasan <pc.kashiwasan@gmail.com>
2012-01-20 19:51:51 +0900

    fixed style sheet (3=>4) for smartphone (refs #2227)

--- apps/pc_frontend/modules/community/templates/_smtCommunityMemberJoinListBox.php
+++ apps/pc_frontend/modules/community/templates/_smtCommunityMemberJoinListBox.php
@@ -1,8 +1,8 @@
 <?php use_helper('Javascript') ?>
 <script id="communityMemberJoinListTemplate" type="text/x-jquery-tmpl">
   <div class="span3">
-    <div class="row_memberimg row"><a href="${profile_url}"><img src="${profile_image}" class="rad10" width="57" height="57"></a></div>
-    <div class="row_membername font10 row"><a href="${profile_url}">${name}</a> (${friends_count})</div>
+    <div class="row_memberimg row"><div class="span3 center"><a href="${profile_url}"><img src="${profile_image}" class="rad10" width="57" height="57"></a></div></div>
+    <div class="row_membername font10 row"><div class="span3 center"><a href="${profile_url}">${name}</a> (${friends_count})</div></div>
   </div>
 </script>
 <script type="text/javascript">
