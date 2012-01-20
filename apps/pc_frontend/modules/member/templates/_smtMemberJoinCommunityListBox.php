661c2151f625d4530b459fe87532d5623d041d04
kashiwasan <pc.kashiwasan@gmail.com>
2012-01-20 20:00:32 +0900

    remove style 'margin-left: 0px;' (refs #2742)

--- apps/pc_frontend/modules/member/templates/_smtMemberJoinCommunityListBox.php
+++ apps/pc_frontend/modules/member/templates/_smtMemberJoinCommunityListBox.php
@@ -1,8 +1,8 @@
 <?php use_helper('Javascript') ?>
 <script id="joinCommunityListTemplate" type="text/x-jquery-tmpl">
   <div class="span3">
-    <div class="row_memberimg row"><a href="${community_url}"><img src="${community_image_url}" class="rad10" width="57" height="57"></a></div>
-    <div class="row_membername font10 row"><a href="${community_url}">${name}</a> (${member_count})</div>
+    <div class="row_memberimg row"><div class="span3 center"><a href="${community_url}"><img src="${community_image_url}" class="rad10" width="57" height="57"></a></div></div>
+    <div class="row_membername font10 row"><div class="span3 center"><a href="${community_url}">${name}</a> (${member_count})</div></div>
   </div>
 </script>
 <script type="text/javascript">
@@ -20,7 +20,7 @@ $(function(){
   <div class="gadget_header span12"><?php echo __('%community% List', array('%community%' => $op_term['community'])) ?></div>
 </div>
 <hr class="toumei" />
-<div class="row hide" id="memberJoinCommunityList" style="margin-left: 0;">
+<div class="row hide" id="memberJoinCommunityList">
 </div>
 <div class="row" id="memberJoinCommunityListLoading" style="margin-left: 0; text-align: center;">
 <?php echo op_image_tag('ajax-loader.gif', array()) ?>
