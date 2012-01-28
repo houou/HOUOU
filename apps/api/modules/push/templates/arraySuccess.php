8e47bbd8c14fa9283bf4c3da1ac9503b343576fa
Kimura Youichi <kim.upsilon@bucyou.net>
2012-01-28 19:53:35 +0900

    fix push/search.json

--- apps/api/modules/push/templates/arraySuccess.php
+++ apps/api/modules/push/templates/arraySuccess.php
@@ -4,15 +4,7 @@ $data = array();
 
 foreach ($notifications as $notification)
 {
-  $data[] = array(
-    'body' => $notification['body'],
-    'category' => $notification['category'],
-    'unread' => $notification['unread'],
-    'created_at' => date('r', strtotime($notification['created_at'])),
-    'icon_url' => sf_image_path($notification['icon_url'], array('size' => '48x48'), true),
-    'url' => $notification['url'] ? url_for($notification['url'], array('abstract' => true)) : null,
-    'member_id_from' => $notification['member_id_from'],
-  );
+  $data[] = op_api_notification($notification);
 }
 
 return array(
